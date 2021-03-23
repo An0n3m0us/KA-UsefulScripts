dca71441b61f1b0d8812a82958cf5ab45f7415b0
An0n3m0us <an0n3m0us@hotmail.com>
2021-03-23 22:32:42 +0000

    Add DGPE project script

---
+++ dgpeProjects.js
@@ -0,0 +1,68 @@
+// Bookmark the one-liner code below!
+javascript:var%20%24jscomp%3D%24jscomp%7C%7C%7B%7D%3B%24jscomp.scope%3D%7B%7D%3B%24jscomp.createTemplateTagFirstArg%3Dfunction(a)%7Breturn%20a.raw%3Da%7D%3B%24jscomp.createTemplateTagFirstArgWithRaw%3Dfunction(a%2Cb)%7Ba.raw%3Db%3Breturn%20a%7D%3B1%3Cdocument.getElementsByClassName(%22_17vrefge%22).length%26%26(document.getElementsByClassName(%22_17vrefge%22)%5B0%5D.parentNode.removeChild(document.getElementsByClassName(%22_17vrefge%22)%5B0%5D)%2Cdocument.getElementsByClassName(%22_duavrzj%22)%5B0%5D.outerHTML%3D'%3Cinput%20id%3D%22dateInput%22%20type%3D%22date%22%3E%3Cp%20id%3D%22cursor%22%3E%3C%2Fp%3E'%2Cdocument.getElementById(%22dateInput%22).valueAsDate%3Dnew%20Date%2Cshowcase%3Ddocument.getElementsByClassName(%22_y9ev9r%22)%5B0%5D%2Cshowcase.innerHTML%3D%22%22%2Cbutton_old%3Ddocument.getElementsByClassName(%22_qaciyjd%22)%5B0%5D%2Cbutton%3Ddocument.getElementsByClassName(%22_qaciyjd%22)%5B0%5D.cloneNode(!0)%2Cbutton_old.parentNode.replaceChild(button%2Cbutton_old)%2Cbutton%3Ddocument.getElementsByClassName(%22_qaciyjd%22)%5B0%5D%2CcursorNext%3D%22%22)%3Bfunction%20addProject(a%2Cb%2Cc)%7Bvote%3D1%3D%3Da.sumVotesIncremented%3F%22Vote%22%3A%22Votes%22%3Bspinoff%3D1%3D%3Da.spinoffCount%3F%22Spin-Off%22%3A%22Spin-Offs%22%3Bcreated%3Db.created.split(%22T%22)%5B0%5D%2B%22%20%5Cu00b7%20%22%2Bc.feedback.length%2B%22%20T%26T%22%3Bshowcase.innerHTML%2B%3D'%3Cdiv%20class%3D%22_eof4m4b%22%3E%5Cn%5Ct%3Cdiv%20class%3D%22_xu2jcg%22%3E%5Cn%5Ct%5Ct%3Ca%20href%3D%22'%2Ba.url%2B'%22%20target%3D%22_blank%22%3E%5Cn%5Ct%5Ct%5Ct%3Cimg%20class%3D%22_un0iag%22%20alt%3D%22%22%20src%3D%22'%2Ba.thumb%2B'%22%3E%5Cn%5Ct%5Ct%5Ct%3C%2Fa%3E%5Cn%5Ct%5Ct%5Ct%3Ca%20tabindex%3D%220%22%20href%3D%22'%2Ba.url%2B'%22%20class%3D%22_1g8isxy8%22%3E%5Cn%5Ct%5Ct%5Ct%5Ct%3Cspan%20class%3D%22_w2mdcoh%22%3E'%2Ba.title%2B'%3C%2Fspan%3E%5Cn%5Ct%5Ct%5Ct%3C%2Fa%3E%5Cn%5Ct%5Ct%3C%2Fdiv%3E%5Cn%5Ct%5Ct%3Ca%20tabindex%3D%220%22%20href%3D%22%2Fprofile%2F'%2Ba.authorKaid%2B'%22%20class%3D%22_1g8isxy8%22%3E%5Cn%5Ct%5Ct%5Ct%3Cspan%20class%3D%22_11pfpjyh%22%3E'%2Ba.authorNickname%2B'%3C%2Fspan%3E%5Cn%5Ct%5Ct%3C%2Fa%3E%5Cn%5Ct%5Ct%3Cdiv%20class%3D%22_kt2szrr%22%3E'%2Bcreated%2B%22%3Cbr%3E%22%2Ba.sumVotesIncremented%2B%22%20%22%2Bvote%2B%22%20%5Cu00b7%20%22%2Ba.spinoffCount%2B%22%20Spin-Offs%3C%2Fdiv%3E%5Cn%5Ct%3C%2Fdiv%3E%22%7Dfunction%20getData(a)%7Bconsole.log(cursorNext)%3Bbutton.setAttribute(%22disabled%22%2C%22true%22)%3B%24.getJSON(%22https%3A%2F%2Fwww.khanacademy.org%2Fapi%2Finternal%2Fscratchpads%2Ftop%3Fcasing%3Dcamel%26sort%3D2%26limit%3D30%26subject%3Dall%26topic_id%3Dxffde7c31%26curriculum%3D1%22%2Ba%2B%22%26callback%3D%3F%22%2Cfunction(b)%7BcursorNext%3D%22%26cursor%3D%22%2Bb.cursor%3Bnew%20Date(b.scratchpads%5B0%5D.created.split(%22T%22)%5B0%5D)%3C%3Dnew%20Date(document.getElementById(%22dateInput%22).value)%3FObject.values(b.scratchpads).forEach(function(c%2Ce)%7BsetTimeout(function()%7B%24.getJSON(%22https%3A%2F%2Fwww.khanacademy.org%2Fapi%2Flabs%2Fscratchpads%2F%22%2Bc.url.split(%22%2F%22).slice(-1)%5B0%5D%2B%22%3Fcallback%3D%3F%22%2Cfunction(d)%7B%24.getJSON(%22https%3A%2F%2Fwww.khanacademy.org%2Fapi%2Finternal%2Fdiscussions%2Fscratchpad%2F%22%2Bc.url.split(%22%2F%22).slice(-1)%5B0%5D%2B%22%2Fcomments%3Fcallback%3D%3F%22%2Cfunction(f)%7B%22pjs%22%3D%3D%3Dd.userAuthoredContentType%26%26addProject(c%2Cd%2Cf)%7D)%7D)%7D%2C50*(e%2B1))%7D)%3AgetData(cursorNext)%3Bdocument.getElementById(%22cursor%22).innerText%3Db.scratchpads%5Bb.scratchpads.length-1%5D.created.slice(0%2C-1).split(%22T%22).join(%22%20%22)%3Bbutton.removeAttribute(%22disabled%22)%7D)%7Dbutton.addEventListener(%22click%22%2Cfunction(a)%7BgetData(cursorNext)%3Bdocument.getElementById(%22dateInput%22).style.display%3D%22none%22%7D)%3Bvoid+0
+
+
+
+if (document.getElementsByClassName("_17vrefge").length > 1) {
+  document.getElementsByClassName("_17vrefge")[0].parentNode.removeChild(document.getElementsByClassName("_17vrefge")[0])
+  document.getElementsByClassName("_duavrzj")[0].outerHTML = "<input id=\"dateInput\" type=\"date\"><p id=\"cursor\"></p>";
+  document.getElementById("dateInput").valueAsDate = new Date();
+  showcase = document.getElementsByClassName("_y9ev9r")[0];
+  showcase.innerHTML = "";
+  button_old = document.getElementsByClassName("_qaciyjd")[0];
+  button = document.getElementsByClassName("_qaciyjd")[0].cloneNode(true);
+  button_old.parentNode.replaceChild(button, button_old);
+  button = document.getElementsByClassName("_qaciyjd")[0];
+  cursorNext = "";
+}
+
+function addProject(project, apidata, discussion) {
+  vote = (project.sumVotesIncremented == 1 ? "Vote" : "Votes");
+  spinoff = (project.spinoffCount == 1 ? "Spin-Off" : "Spin-Offs");
+  created = apidata.created.split("T")[0] + " · " + discussion.feedback.length + " T&T";
+
+  showcase.innerHTML = showcase.innerHTML + `<div class="_eof4m4b">
+	<div class="_xu2jcg">
+		<a href="${project.url}" target="_blank">
+			<img class="_un0iag" alt="" src="${project.thumb}">
+			</a>
+			<a tabindex="0" href="${project.url}" class="_1g8isxy8">
+				<span class="_w2mdcoh">${project.title}</span>
+			</a>
+		</div>
+		<a tabindex="0" href="/profile/${project.authorKaid}" class="_1g8isxy8">
+			<span class="_11pfpjyh">${project.authorNickname}</span>
+		</a>
+		<div class="_kt2szrr">${created}<br>${project.sumVotesIncremented} ${vote} · ${project.spinoffCount} Spin-Offs</div>
+	</div>`
+}
+
+function getData(cursor) {
+  console.log(cursorNext);
+  button.setAttribute("disabled", "true");
+  $.getJSON(`https://www.khanacademy.org/api/internal/scratchpads/top?casing=camel&sort=2&limit=30&subject=all&topic_id=xffde7c31&curriculum=1${cursor}&callback=?`, function (apidata) {
+      cursorNext = "&cursor=" + apidata.cursor;
+      if (new Date(apidata.scratchpads[0].created.split("T")[0]) <= new Date(document.getElementById("dateInput").value)) {
+        Object.values(apidata.scratchpads).forEach(function(project, index){
+            setTimeout(function(){
+              $.getJSON(`https://www.khanacademy.org/api/labs/scratchpads/${project.url.split("/").slice(-1)[0]}?callback=?`, function (apidata) {
+                $.getJSON(`https://www.khanacademy.org/api/internal/discussions/scratchpad/${project.url.split("/").slice(-1)[0]}/comments?callback=?`, function (discussion) {
+                  if (apidata.userAuthoredContentType === "pjs") {
+                    addProject(project, apidata, discussion);
+                  }
+                });
+              });
+            }, 50 * (index+1));
+        });
+      } else {
+          getData(cursorNext);
+      }
+      document.getElementById("cursor").innerText = apidata.scratchpads[apidata.scratchpads.length-1].created.slice(0, -1).split("T").join(" ");
+      button.removeAttribute("disabled");
+  });
+}
+
+button.addEventListener("click", function(e) {
+    getData(cursorNext);
+    document.getElementById("dateInput").style.display = "none";
+});
